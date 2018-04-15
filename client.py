bf23e512bc3ac1ab07dcfac727943f1e5efb6b9e
Pol_M <polmarcetsarda@gmail.com>
2018-04-15 13:09:44 +0200

    Primer commit. Res important, simplement he afegit els primers scripts. Canvis a cometre: 	modificat:        .gitignore 	fitxer nou:       client.py 	fitxer nou:       history.sh 	fitxer nou:       server.py

---
+++ client.py
@@ -0,0 +1,29 @@
+#!/bin/python2
+# -*- coding: utf-8 -*-
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+Working = True
+# Connect the socket to the port where the server is listening
+server_address = ('localhost', 10000)
+print >>sys.stderr, 'connecting to %s port %s' % server_address
+sock.connect(server_address)
+try:
+    while Working:
+        # Send data
+        message = 'This is the message.  It will be repeated.'
+        print >>sys.stderr, 'sending "%s"' % message
+        sock.sendall(message)
+
+        # Look for the response
+        amount_received = 0
+        amount_expected = len(message)
+        data = sock.recv(2048)
+        amount_received += len(data)
+        print >>sys.stderr, 'received "%s"' % data
+
+finally:
+        print >>sys.stderr, 'closing socket'
+        sock.close()
---
+++ history.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+cat testdoc.txt | gpg -ea -vv -R polmarcetsarda@gmail.com > testdoc.txt.asc
+cat testdoc.txt.asc | gpg -d -vv > testdoc.txt
+cat > template <<EOF
+     %echo Generating a basic OpenPGP key
+     Key-Type: default
+     Key-Length: 4096
+     Subkey-Type: default
+     Subkey-Length: 4096
+     Name-Real: Test Boy
+     Name-Comment: Sóc una proba simpàtica
+     Name-Email: test@encriptador.pol
+     Expire-Date: 0
+     Passphrase: contra_segura
+     # Do a commit here, so that we can later print "done" :-)
+     %commit
+     %echo done
+EOF
+gpg --batch --full-gen-key template
