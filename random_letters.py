8ee5c4a14164fb38d8127a58165f4e1c070b4fc4
Pol_M <polmarcetsarda@gmail.com>
2018-05-20 14:28:52 +0200

    estic cansat d'inventar-me alfabets random

---
+++ random_letters.py
@@ -0,0 +1,9 @@
+#!/bin/python
+import string
+import random
+letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+random.shuffle(letters)
+alpha=""
+for letter in letters:
+ alpha+=letter
+print(alpha)
